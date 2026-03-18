de9944a5ce549c12ecb2b60135e9aa4bced2f00d
Habibullo <acccisco3@gmail.com>
2026-03-18 13:06:20 +0500

    change

--- apps/course/admin.py
+++ apps/course/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from apps.course.models.category import Category
 from apps.course.models.course import Course
 from apps.course.models.lessons import Lessons
 from apps.course.models.student import CourseStudent
+from apps.course.models.units import CourseUnit
 
 # Register your models here.
 
@@ -10,3 +12,7 @@ from apps.course.models.student import CourseStudent
 admin.site.register(Course)
 admin.site.register(CourseStudent)
 admin.site.register(Lessons)
+
+admin.site.register(Category)
+
+admin.site.register(CourseUnit)
--- apps/quiz/admin.py
+++ apps/quiz/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from apps.course.models.units import CourseUnit
+from apps.quiz.models import Quiz, Questions, QuizResult, Variant
+
 # Register your models here.
+
+
+admin.site.register(Quiz)
+admin.site.register(Questions)
+admin.site.register(QuizResult)
+admin.site.register(Variant)
+
