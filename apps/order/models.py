361a57bd39c1e2aecfb7895de019f20246b21ac6
Habibullo <acccisco3@gmail.com>
2026-03-21 09:31:06 +0500

    order updated!

--- apps/order/models.py
+++ apps/order/models.py
@@ -18,7 +18,7 @@ class OrderStatus(TextChoices):
 class Order(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    status = models.CharField(max_length=25, choices=OrderStatus.choices, default=OrderStatus.PENDING)
+    status = models.CharField(max_length=25, choices=OrderStatus.choices, default=OrderStatus.PAID)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
 
