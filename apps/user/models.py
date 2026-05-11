75567916c60dbb0b8f27d250fb70e97863601ed8
Habibullo <acccisco3@gmail.com>
2026-05-11 14:36:39 +0500

    updated changes

--- apps/user/models.py
+++ apps/user/models.py
@@ -54,6 +54,7 @@ class User(AbstractUser, BaseModel):
     role = models.CharField(max_length=10, choices=UserRoles.choices, default=UserRoles.USER)
 
 
+
     has_real_password = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
